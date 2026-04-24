b1acbfad5efb0bb16b0ff663405901d2a6faab69
postex-adnan <postex-adnan@example.com>
2026-04-24 14:10:15 +0500

    chore: initial commit

---
+++ apps/mobile/metro.config.js
@@ -0,0 +1,27 @@
+const path = require('path');
+const { getDefaultConfig, mergeConfig } = require('@react-native/metro-config');
+
+const projectRoot = __dirname;
+const workspaceRoot = path.resolve(projectRoot, '..', '..');
+
+/**
+ * Metro configuration
+ * https://reactnative.dev/docs/metro
+ *
+ * @type {import('@react-native/metro-config').MetroConfig}
+ */
+const config = {
+  projectRoot,
+  watchFolders: [workspaceRoot],
+  resolver: {
+    nodeModulesPaths: [
+      path.resolve(projectRoot, 'node_modules'),
+      path.resolve(workspaceRoot, 'node_modules'),
+    ],
+    extraNodeModules: new Proxy({}, {
+      get: (target, name) => path.join(workspaceRoot, 'node_modules', name),
+    }),
+  },
+};
+
+module.exports = mergeConfig(getDefaultConfig(projectRoot), config);
